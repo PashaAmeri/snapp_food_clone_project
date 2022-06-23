fa6f55e5a6abaad13f04c5ea6241297814e121c9
PashaAmeri <a.pasha.ameri081@gmail.com>
2022-06-24 01:28:50 +0430

    created food table migration and model

---
+++ database/migrations/2022_06_23_135624_create_food_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\FoodCategory;
+use App\Models\Restaurant;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Restaurant::class);
+            $table->foreignIdFor(FoodCategory::class);
+            $table->string('food_name');
+            $table->integer('food_price');
+            $table->text('food_description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food');
+    }
+};
