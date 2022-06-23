a5b259e89cd63f8a3a892c780e68f42d470676a1
PashaAmeri <a.pasha.ameri081@gmail.com>
2022-06-24 01:25:50 +0430

    created user migration and model

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('name');
+            $table->string('phone')->unique()->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->tinyInteger('role')->default(2);
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
