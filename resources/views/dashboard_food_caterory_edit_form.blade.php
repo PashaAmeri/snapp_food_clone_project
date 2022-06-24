51828856561ca81f1c7a998d59323db36e9f9010
PashaAmeri <a.pasha.ameri081@gmail.com>
2022-06-24 19:23:14 +0430

    errors added to food category edit form

--- resources/views/dashboard_food_caterory_edit_form.blade.php
+++ resources/views/dashboard_food_caterory_edit_form.blade.php
@@ -16,7 +16,7 @@
       
           <!-- Header & Price -->
           <p class="text-3xl text-gray-700 font-semibold">{{ $food_category->category_name }}</p>
-          <p class="text-xl text-blue-700 font-semibold mt-1"> Food category </p>
+          <p class="text-xl text-blue-700 font-semibold mt-1">Food category</p>
           <p class="text-sm text-gray-500 font-semibold mt-1">Last change: {{ $food_category->updated_at }}</p>
       
           <!-- Description -->
@@ -48,6 +48,16 @@
 
               <x-jet-label for="name" value="{{ __('Category name') }}" />
               <x-jet-input name="category_name" id="name" type="text" value="{{ old('category_name') ?? $food_category->category_name }}" class="mt-1 block w-full" wire:model.defer="state.email" />
+                
+                @error('category_name')
+
+                <div>
+
+                  <span class="text-red-700">{{ $message }}</span>
+
+                </div>
+
+                @enderror
 
             </div>
 
@@ -55,6 +65,16 @@
 
               <x-jet-label for="description" value="{{ __('Description') }}" />
               <textarea id="description" rows="4" name="category_description" class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full" placeholder="Type here...">{{ old('category_description') ?? $food_category->category_description }}</textarea>
+
+              @error('category_description')
+
+              <div>
+
+                <span class="text-red-700">{{ $message }}</span>
+
+              </div>
+
+              @enderror
 
             </div>
 
