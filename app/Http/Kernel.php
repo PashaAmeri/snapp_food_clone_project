b114cc78568f374ec3d8ad5ecd32c4b0ef3f1f62
PashaAmeri <a.pasha.ameri081@gmail.com>
2022-06-29 16:37:04 +0430

    added seller and admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'not_user' => \App\Http\Middleware\NotUser::class,
+        'admin_access' => \App\Http\Middleware\AdminAccess::class,
+        'seller_access' => \App\Http\Middleware\SellerAccess::class,
     ];
 }
