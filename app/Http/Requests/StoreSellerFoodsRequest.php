e6face300d373a158f0db6094bad3e4578bb32f2
PashaAmeri <a.pasha.ameri081@gmail.com>
2022-06-30 23:26:36 +0430

    created request validation for foods controller

---
+++ app/Http/Requests/StoreSellerFoodsRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSellerFoodsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+
+        return [
+            'food_category_id' => 'required|min:1|max:64|exists:food_categories,id',
+            'food_name' => 'required|min:1|max:64',
+            'food_price' => 'required|numeric|min:1|max:9999999',
+            'food_ingredients' => 'nullable|min:1|max:300',
+            'food_description' => 'required|min:1|max:300',
+        ];
+    }
+}
