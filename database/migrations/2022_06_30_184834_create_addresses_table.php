c9c3a8529a1a320f8d3a0e50ea7798bb7978f1cb
PashaAmeri <a.pasha.ameri081@gmail.com>
2022-06-30 23:35:05 +0430

    addresses table model and migration created

---
+++ database/migrations/2022_06_30_184834_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->string('title')->nullable();
+            $table->text('address');
+            $table->decimal('latitude');
+            $table->decimal('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
