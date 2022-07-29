15cd9ff5e652d6edc1c00f09b4cdbbd5404b36f2
PashaAmeri <a.pasha.ameri081@gmail.com>
2022-07-29 09:39:08 +0430

    fixed typo

--- resources/views/seller_profile/dashboard_profile_create_form.blade.php
+++ resources/views/seller_profile/dashboard_profile_create_form.blade.php
@@ -3,7 +3,7 @@
   <x-slot name="header">
 
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Compelit Your Restaurant Profile') }}
+      {{ __('Compelet Your Restaurant Profile') }}
     </h2>
 
   </x-slot>
